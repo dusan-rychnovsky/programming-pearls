8f9784e66a4a0858c38c3719e4d7135c2b9035df
Dušan Rychnovský <drychnovsky@casenetllc.com>
2015-04-28 15:04:36 +0200

    CH02 - EC - Replace the example-based test with a property-based one.

--- src/test/java/cz/dusanrychnovsky/programmingpearls/ch02/eC/FindAnagramsTest.java
+++ src/test/java/cz/dusanrychnovsky/programmingpearls/ch02/eC/FindAnagramsTest.java
@@ -1,35 +1,99 @@
 package cz.dusanrychnovsky.programmingpearls.ch02.eC;
 
+import com.google.common.io.Files;
+import lombok.Value;
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.List;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.charset.Charset;
+import java.util.*;
 
 import static java.util.Arrays.asList;
-import static junit.framework.Assert.assertTrue;
-import static org.junit.Assert.assertEquals;
 
 public class FindAnagramsTest {
 
-    private FindAnagrams impl = new SortingBasedFindAnagrams();
+    private final List<FindAnagrams> implementations = asList(new SortingBasedFindAnagrams());
+    private final List<TestCase> testCases;
 
-    @Test
-    public void initialTest() {
+    public FindAnagramsTest() throws URISyntaxException, IOException {
 
-        List<String> words = asList(
-            "pots",
-            "microphotographics",
-            "photomicrographics",
-            "stop",
-            "tops"
-        );
+        File dir = new File(getClass().getResource("words").toURI());
+        testCases = new LinkedList<>();
 
-        List<List<String>> groups = impl.apply(words);
-
-        assertEquals(2, groups.size());
-        assertTrue(groups.contains(asList("pots", "stop", "tops")));
-        assertTrue(groups.contains(asList("microphotographics", "photomicrographics")));
+        for (File file : dir.listFiles()) {
+            List<String> words = Files.readLines(file, Charset.forName("UTF-8"));
+            testCases.add(new TestCase(words));
+        }
     }
 
+    @Test
+    public void check() {
 
+        for (FindAnagrams implementation : implementations) {
+            for (TestCase testCase : testCases) {
+                List<String> words = testCase.getWords();
+                List<List<String>> result = implementation.apply(words);
+
+                checkThatResultContainsAllOriginalWords(result, words);
+                checkThatTheGroupsAreFormedByMutualAnagrams(result);
+            }
+        }
+    }
+
+    private void checkThatTheGroupsAreFormedByMutualAnagrams(List<List<String>> result) {
+
+        for (List<String> group : result) {
+            checkThatTheGroupIsFormedByMutualAnagrams(group);
+        }
+    }
+
+    private void checkThatTheGroupIsFormedByMutualAnagrams(List<String> group) {
+
+        Iterator<String> groupIt = group.iterator();
+        String firstWord = groupIt.next();
+
+        while (groupIt.hasNext()) {
+            String currWord = groupIt.next();
+            if (!areAnagrams(firstWord, currWord)) {
+                throw new AssertionError(
+                    "Words [" + firstWord + "] and [" + currWord + "] are not anagrams."
+                );
+            }
+        }
+    }
+
+    private boolean areAnagrams(String first, String second) {
+        return sort(first).equals(sort(second));
+    }
+
+    private String sort(String word) {
+        char[] chars = word.toCharArray();
+        Arrays.sort(chars);
+        return new String(chars);
+    }
+
+    private void checkThatResultContainsAllOriginalWords(List<List<String>> result, List<String> words) {
+
+        for (String word : words) {
+
+            boolean present = false;
+            for (List<String> group : result) {
+                if (group.contains(word)) {
+                    present = true;
+                    break;
+                }
+            }
+
+            if (!present) {
+                throw new AssertionError("Word [" + word + "] does not appear in any group.");
+            }
+        }
+    }
+
+    @Value
+    private static class TestCase {
+        private List<String> words;
+    }
 }
